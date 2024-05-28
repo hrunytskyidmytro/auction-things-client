19051b43553e485773640ebbf25d1d0fe49a4e5e
”Dmytro <ipz203_gds@student.ztu.edu.ua>
2024-05-28 23:39:37 +0300

    Updated main store index

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userApi } from "../api/userApi";
 import { passwordResetApi } from "../api/passwordResetApi";
 import { lotApi } from "../api/lotApi";
+import { categoryApi } from "../api/categoryApi";
+import { bidApi } from "../api/bidApi";
 import { authSlice } from "./authSlice";
 
 export const store = configureStore({
@@ -9,12 +11,16 @@ export const store = configureStore({
     [userApi.reducerPath]: userApi.reducer,
     [passwordResetApi.reducerPath]: passwordResetApi.reducer,
     [lotApi.reducerPath]: lotApi.reducer,
+    [categoryApi.reducerPath]: categoryApi.reducer,
+    [bidApi.reducerPath]: bidApi.reducer,
     [authSlice.name]: authSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(userApi.middleware)
       .concat(passwordResetApi.middleware)
-      .concat(lotApi.middleware),
+      .concat(lotApi.middleware)
+      .concat(bidApi.middleware)
+      .concat(categoryApi.middleware),
   devTools: process.env.NODE_ENV !== "production",
 });
