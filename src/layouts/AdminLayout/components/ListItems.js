a51a9d4e8f3907cb7fcdb1bfa499e75e3a10bd44
”Dmytro <ipz203_gds@student.ztu.edu.ua>
2024-05-30 02:44:30 +0300

    Updated main menu ia admin panel

--- src/layouts/AdminLayout/components/ListItems.js
+++ src/layouts/AdminLayout/components/ListItems.js
@@ -11,7 +11,11 @@ import PeopleIcon from "@mui/icons-material/People";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import GavelIcon from "@mui/icons-material/Gavel";
 import CategoryIcon from "@mui/icons-material/Category";
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import RateReviewIcon from "@mui/icons-material/RateReview";
+import PaymentsOutlinedIcon from "@mui/icons-material/PaymentsOutlined";
+import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import HistoryEduOutlinedIcon from "@mui/icons-material/HistoryEduOutlined";
 
 import { useNavigate } from "react-router-dom";
 
@@ -41,14 +45,34 @@ export const mainListItems = (
       />
       <MainListItem to="/admin/lots" primary="Лоти" icon={<GavelIcon />} />
       <MainListItem
-        to="/admin/categories"
-        primary="Категорії"
-        icon={<CategoryIcon />}
+        to="/admin/auction-history"
+        primary="Історія лотів"
+        icon={<HistoryEduOutlinedIcon />}
       />
       <MainListItem
         to="/admin/bids"
         primary="Ставки"
         icon={<AttachMoneyIcon />}
+      />
+      <MainListItem
+        to="/admin/payments"
+        primary="Платежі"
+        icon={<PaymentsOutlinedIcon />}
+      />
+      <MainListItem
+        to="/admin/transactions"
+        primary="Транзакції"
+        icon={<PaidOutlinedIcon />}
+      />
+      <MainListItem
+        to="/admin/categories"
+        primary="Категорії"
+        icon={<CategoryIcon />}
+      />
+      <MainListItem
+        to="/admin/reviews"
+        primary="Відгуки"
+        icon={<RateReviewIcon />}
       />
     </>
   </>
