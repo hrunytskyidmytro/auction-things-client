984c2750d81851bf0bdcb82b0e9868885314c171
”Dmytro <ipz203_gds@student.ztu.edu.ua>
2024-06-02 02:48:00 +0300

    Connection to the main store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { userApi } from "../api/userApi";
-import { passwordResetApi } from "../api/passwordResetApi";
-import { lotApi } from "../api/lotApi";
-import { categoryApi } from "../api/categoryApi";
-import { bidApi } from "../api/bidApi";
-import { watchlistApi } from "../api/watchlistApi";
+import { api } from "../api/api";
 import { authSlice } from "./authSlice";
 
 export const store = configureStore({
   reducer: {
-    [userApi.reducerPath]: userApi.reducer,
-    [passwordResetApi.reducerPath]: passwordResetApi.reducer,
-    [lotApi.reducerPath]: lotApi.reducer,
-    [categoryApi.reducerPath]: categoryApi.reducer,
-    [bidApi.reducerPath]: bidApi.reducer,
-    [watchlistApi.reducerPath]: watchlistApi.reducer,
+    [api.reducerPath]: api.reducer,
     [authSlice.name]: authSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(userApi.middleware)
-      .concat(passwordResetApi.middleware)
-      .concat(lotApi.middleware)
-      .concat(bidApi.middleware)
-      .concat(categoryApi.middleware)
-      .concat(watchlistApi.middleware),
+    getDefaultMiddleware().concat(api.middleware),
   devTools: process.env.NODE_ENV !== "production",
 });
