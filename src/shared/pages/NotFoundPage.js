93626f801ecd74e8386033ba3c780e1aef7782e5
”Dmytro <ipz203_gds@student.ztu.edu.ua>
2024-06-08 03:32:03 +0300

    Styled 404 page not found.

--- src/shared/pages/NotFoundPage.js
+++ src/shared/pages/NotFoundPage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "../styles/NotFoundPageStyle.css";
 
 const NotFoundPage = () => {
-  return <>Not Found Page work!</>;
+  return (
+    <>
+      <div id="notfound">
+        <div className="notfound">
+          <div className="notfound-404">
+            <h1>404</h1>
+          </div>
+          <h2>Вибачте, сторінка не знайдена!</h2>
+          <p>
+            Сторінка, яку ви шукаєте, могла бути видалена, якщо її назва або
+            вона тимчасово недоступна.
+          </p>
+          <a href="/">Повернутися на головну</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFoundPage;
