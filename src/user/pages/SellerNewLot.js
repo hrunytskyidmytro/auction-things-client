0bb6ca1112c75f17599eab2d2a958a6c9b82642f
”Dmytro <ipz203_gds@student.ztu.edu.ua>
2024-06-12 02:55:52 +0300

    Changes, added the ability to add and edit lots for the seller.

---
+++ src/user/pages/SellerNewLot.js
@@ -0,0 +1,347 @@
+import React, { useState, useEffect } from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import {
+  TextField,
+  FormControl,
+  InputAdornment,
+  Box,
+  InputLabel,
+  Select,
+  MenuItem,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "@ckeditor/ckeditor5-build-classic/build/translations/uk";
+import { Dropzone, FileMosaic } from "@files-ui/react";
+import { useNavigate } from "react-router-dom";
+import { useCreateLotMutation } from "../../api/lotApi";
+import { validationSchemaForNewLot } from "../../shared/utils/validatorsSchemes";
+import { useGetAllCategoriesQuery } from "../../api/categoryApi";
+import { useAuth } from "../../shared/hooks/useAuth";
+import MessageSnackbar from "../../shared/components/UIElements/MessageSnackbar";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import Breadcrumbs from "../../shared/components/UIElements/Breadcrumbs";
+
+const UserNewLot = () => {
+  const [createLot, { isLoading, error }] = useCreateLotMutation();
+  const {
+    data: categoriesData,
+    isLoading: isCategoriesLoading,
+    error: categoriesError,
+  } = useGetAllCategoriesQuery();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  const [files, setFiles] = useState([]);
+  const [openSuccessAlert, setOpenSuccessAlert] = useState(false);
+  const [openErrorAlert, setOpenErrorAlert] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (categoriesError) {
+      setOpenErrorAlert(true);
+    }
+  }, [categoriesError]);
+
+  const updateFiles = (incommingFiles) => {
+    setFiles(incommingFiles);
+  };
+
+  const removeFile = (id) => {
+    setFiles(files.filter((x) => x.id !== id));
+  };
+
+  if (isCategoriesLoading) {
+    return <LoadingSpinner size={30} />;
+  }
+
+  return (
+    <>
+      <Container
+        id="seller_new_lot"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: { xs: 3, sm: 6 },
+        }}
+      >
+        <Box
+          sx={{
+            width: { sm: "100%", md: "100%" },
+          }}
+        >
+          <Grid container spacing={3} justifyContent="center">
+            <Grid item xs={12} sx={{ mt: 15 }}>
+              <Breadcrumbs
+                links={[
+                  { label: "Лоти", url: "/lots" },
+                  { label: "Додавання нового лоту", url: `/lots/seller/new` },
+                ]}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Box
+                sx={{
+                  my: 5,
+                  mx: 4,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Typography variant="body2" textAlign={"center"}>
+                  При редагуванні, зображення змінювати не можна. Намагайтеся
+                  зображення встановлювати на початку.
+                </Typography>
+                <Formik
+                  initialValues={{
+                    title: "",
+                    description: "",
+                    startingPrice: 0.0,
+                    endDate: "",
+                    status: "OPEN",
+                    categoryId: "",
+                    buyNowPrice: 0.0,
+                    bidIncrement: 0.0,
+                    reservePrice: 0.0,
+                  }}
+                  validationSchema={validationSchemaForNewLot}
+                  onSubmit={async (values, { setSubmitting }) => {
+                    try {
+                      const formData = new FormData();
+                      formData.append("title", values.title);
+                      formData.append("description", description);
+                      formData.append("startingPrice", values.startingPrice);
+                      formData.append("endDate", values.endDate);
+                      formData.append("status", "OPEN");
+                      formData.append("categoryId", values.categoryId);
+                      formData.append("buyNowPrice", values.buyNowPrice);
+                      formData.append("bidIncrement", values.bidIncrement);
+                      formData.append("reservePrice", values.reservePrice);
+
+                      if (files.length > 0) {
+                        files.forEach((file) => {
+                          formData.append("images", file.file);
+                        });
+                      }
+
+                      const response = await createLot(formData).unwrap();
+                      setOpenSuccessAlert(true);
+                      setSuccessMessage(response.message);
+                      navigate(`/seller/${user.id}`);
+                    } catch (err) {
+                      setOpenErrorAlert(true);
+                    }
+                    setSubmitting(false);
+                  }}
+                >
+                  {({ errors, touched, isSubmitting }) => (
+                    <Form>
+                      <FormControl
+                        fullWidth
+                        margin="normal"
+                        error={touched.imageFiles && Boolean(errors.imageFiles)}
+                      >
+                        <Dropzone
+                          onChange={updateFiles}
+                          value={files}
+                          label={
+                            "Перетягніть файли сюди або натисніть, щоб вибрати"
+                          }
+                          accept={"image/*"}
+                          maxFileSize={28 * 1024 * 1024}
+                          maxFiles={5}
+                          footerConfig={{
+                            customMessage: "Підтримуються типи: png, jpeg, jpg",
+                          }}
+                          fakeUpload
+                        >
+                          {files.map((file) => (
+                            <FileMosaic
+                              key={file.id}
+                              {...file}
+                              onDelete={removeFile}
+                              info
+                              preview
+                            />
+                          ))}
+                        </Dropzone>
+                        <ErrorMessage
+                          name="imageFiles"
+                          component="div"
+                          style={{
+                            color: "red",
+                            fontSize: "0.75rem",
+                            marginTop: "0.25rem",
+                          }}
+                        />
+                      </FormControl>
+                      <Field
+                        as={TextField}
+                        label="Назва"
+                        name="title"
+                        required
+                        margin="normal"
+                        fullWidth
+                        error={touched.title && Boolean(errors.title)}
+                        helperText={<ErrorMessage name="title" />}
+                      />
+                      <CKEditor
+                        editor={ClassicEditor}
+                        config={{
+                          language: "uk",
+                        }}
+                        data="Тут повинен бути опис до лоту (про товар)..."
+                        onReady={(editor) => {
+                          console.log("CKEditor5: ", editor);
+                        }}
+                        onChange={(event, editor) => {
+                          const data = editor.getData();
+                          setDescription(data);
+                        }}
+                      />
+                      <Field
+                        as={TextField}
+                        label="Початкова ціна"
+                        name="startingPrice"
+                        type="number"
+                        margin="normal"
+                        required
+                        fullWidth
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">₴</InputAdornment>
+                          ),
+                        }}
+                        error={
+                          touched.startingPrice && Boolean(errors.startingPrice)
+                        }
+                        helperText={<ErrorMessage name="startingPrice" />}
+                      />
+                      <Field
+                        as={TextField}
+                        label="Дата закінчення"
+                        name="endDate"
+                        type="datetime-local"
+                        required
+                        margin="normal"
+                        fullWidth
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        error={touched.endDate && Boolean(errors.endDate)}
+                        helperText={<ErrorMessage name="endDate" />}
+                      />
+                      <FormControl
+                        fullWidth
+                        margin="normal"
+                        error={touched.categoryId && Boolean(errors.categoryId)}
+                      >
+                        <InputLabel>Категорія</InputLabel>
+                        <Field as={Select} name="categoryId" required>
+                          {categoriesData &&
+                            categoriesData.map((category) => (
+                              <MenuItem key={category.id} value={category.id}>
+                                {category.name}
+                              </MenuItem>
+                            ))}
+                        </Field>
+                      </FormControl>
+                      <Field
+                        as={TextField}
+                        label="Ціна купівлі зараз"
+                        name="buyNowPrice"
+                        type="number"
+                        margin="normal"
+                        required
+                        fullWidth
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">₴</InputAdornment>
+                          ),
+                        }}
+                        error={
+                          touched.buyNowPrice && Boolean(errors.buyNowPrice)
+                        }
+                        helperText={<ErrorMessage name="buyNowPrice" />}
+                      />
+                      <Field
+                        as={TextField}
+                        label="Крок ставки"
+                        name="bidIncrement"
+                        type="number"
+                        margin="normal"
+                        required
+                        fullWidth
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">₴</InputAdornment>
+                          ),
+                        }}
+                        error={
+                          touched.bidIncrement && Boolean(errors.bidIncrement)
+                        }
+                        helperText={<ErrorMessage name="bidIncrement" />}
+                      />
+                      <Field
+                        as={TextField}
+                        label="Резервна ціна"
+                        name="reservePrice"
+                        type="number"
+                        margin="normal"
+                        required
+                        fullWidth
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">₴</InputAdornment>
+                          ),
+                        }}
+                        error={
+                          touched.reservePrice && Boolean(errors.reservePrice)
+                        }
+                        helperText={<ErrorMessage name="reservePrice" />}
+                      />
+                      <LoadingButton
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        loading={isLoading}
+                        disabled={isSubmitting}
+                        sx={{ mt: 3 }}
+                      >
+                        Створити лот
+                      </LoadingButton>
+                    </Form>
+                  )}
+                </Formik>
+              </Box>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+      <MessageSnackbar
+        open={openErrorAlert}
+        onClose={() => setOpenErrorAlert(false)}
+        severity="error"
+        message={
+          error?.data?.message ||
+          "Виникла помилка. Будь ласка, спробуйте ще раз."
+        }
+      />
+      <MessageSnackbar
+        open={openSuccessAlert}
+        onClose={() => setOpenSuccessAlert(false)}
+        severity="error"
+        message={successMessage}
+      />
+    </>
+  );
+};
+
+export default UserNewLot;
---
+++ src/user/pages/SellerUpdateLot.js
@@ -0,0 +1,287 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import {
+  TextField,
+  Box,
+  InputLabel,
+  Select,
+  MenuItem,
+  FormControl,
+  InputAdornment,
+  Container,
+  Grid,
+} from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "@ckeditor/ckeditor5-build-classic/build/translations/uk";
+import { useNavigate } from "react-router-dom";
+import { useGetLotByIdQuery, useUpdateLotMutation } from "../../api/lotApi";
+import { useGetAllCategoriesQuery } from "../../api/categoryApi";
+import { validationSchemaForUpdateLot } from "../../shared/utils/validatorsSchemes";
+import MessageSnackbar from "../../shared/components/UIElements/MessageSnackbar";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import Breadcrumbs from "../../shared/components/UIElements/Breadcrumbs";
+
+const UpdateLot = () => {
+  const { id } = useParams();
+
+  const {
+    data: lot,
+    isLoading: isFetching,
+    error: fetchError,
+    refetch: refetchLot,
+  } = useGetLotByIdQuery(id);
+
+  const [updateLot, { isLoading, error }] = useUpdateLotMutation();
+
+  const {
+    data: categoriesData,
+    isLoading: isCategoriesLoading,
+    error: categoriesError,
+    refetch: refetchCategories,
+  } = useGetAllCategoriesQuery();
+
+  const navigate = useNavigate();
+  const [openSuccessAlert, setOpenSuccessAlert] = useState(false);
+  const [openErrorAlert, setOpenErrorAlert] = useState(false);
+  const [successMessage, setSuccessMessage] = useState("");
+  const [description, setDescription] = useState(lot?.description || "");
+
+  useEffect(() => {
+    if (lot) {
+      refetchLot();
+      refetchCategories();
+      setDescription(lot.description);
+    }
+  }, [lot, refetchLot, refetchCategories]);
+
+  useEffect(() => {
+    if (fetchError || categoriesError) {
+      setOpenErrorAlert(true);
+    }
+  }, [fetchError, categoriesError]);
+
+  if (isFetching || isCategoriesLoading) {
+    return <LoadingSpinner size={30} />;
+  }
+
+  return (
+    <>
+      <Container
+        id="seller_new_lot"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: { xs: 3, sm: 6 },
+        }}
+      >
+        <Box
+          sx={{
+            width: { sm: "100%", md: "100%" },
+          }}
+        >
+          <Grid container spacing={3} justifyContent="center">
+            <Grid item xs={12} sx={{ mt: 15 }}>
+              <Breadcrumbs
+                links={[
+                  { label: "Лоти", url: "/lots" },
+                  { label: lot.title, url: `/lots/${lot.id}` },
+                ]}
+              />
+            </Grid>
+            <Box
+              sx={{
+                my: 5,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Formik
+                initialValues={{
+                  title: lot.title,
+                  description: lot.description,
+                  startingPrice: lot.startingPrice,
+                  endDate: lot.endDate,
+                  categoryId: lot.categoryId,
+                  buyNowPrice: lot.buyNowPrice,
+                  bidIncrement: lot.bidIncrement,
+                  reservePrice: lot.reservePrice,
+                }}
+                validationSchema={validationSchemaForUpdateLot}
+                onSubmit={async (values, { setSubmitting }) => {
+                  try {
+                    const response = await updateLot({
+                      id,
+                      ...values,
+                      description,
+                    }).unwrap();
+                    setOpenSuccessAlert(true);
+                    setSuccessMessage(response.message);
+                    navigate("/admin/lots");
+                  } catch (err) {
+                    setOpenErrorAlert(true);
+                  }
+                  setSubmitting(false);
+                }}
+              >
+                {({ errors, touched, isSubmitting }) => (
+                  <Form>
+                    <Field
+                      as={TextField}
+                      label="Назва"
+                      name="title"
+                      required
+                      margin="normal"
+                      fullWidth
+                      error={touched.title && Boolean(errors.title)}
+                      helperText={<ErrorMessage name="title" />}
+                    />
+                    <CKEditor
+                      editor={ClassicEditor}
+                      config={{ language: "uk" }}
+                      data={description}
+                      onChange={(event, editor) => {
+                        const data = editor.getData();
+                        setDescription(data);
+                      }}
+                    />
+                    <Field
+                      as={TextField}
+                      label="Початкова ціна"
+                      name="startingPrice"
+                      type="number"
+                      required
+                      margin="normal"
+                      fullWidth
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">₴</InputAdornment>
+                        ),
+                      }}
+                      error={
+                        touched.startingPrice && Boolean(errors.startingPrice)
+                      }
+                      helperText={<ErrorMessage name="startingPrice" />}
+                    />
+                    <Field
+                      as={TextField}
+                      label="Дата закінчення"
+                      name="endDate"
+                      type="datetime-local"
+                      required
+                      margin="normal"
+                      fullWidth
+                      InputLabelProps={{ shrink: true }}
+                      error={touched.endDate && Boolean(errors.endDate)}
+                      helperText={<ErrorMessage name="endDate" />}
+                      value={
+                        lot.endDate
+                          ? new Date(lot.endDate).toISOString().slice(0, 16)
+                          : ""
+                      }
+                    />
+                    <FormControl
+                      fullWidth
+                      margin="normal"
+                      error={touched.categoryId && Boolean(errors.categoryId)}
+                    >
+                      <InputLabel>Категорія</InputLabel>
+                      <Field as={Select} name="categoryId" required>
+                        {categoriesData &&
+                          categoriesData.map((category) => (
+                            <MenuItem key={category.id} value={category.id}>
+                              {category.name}
+                            </MenuItem>
+                          ))}
+                      </Field>
+                    </FormControl>
+                    <Field
+                      as={TextField}
+                      label="Ціна купівлі зараз"
+                      name="buyNowPrice"
+                      type="number"
+                      margin="normal"
+                      fullWidth
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">₴</InputAdornment>
+                        ),
+                      }}
+                      error={touched.buyNowPrice && Boolean(errors.buyNowPrice)}
+                      helperText={<ErrorMessage name="buyNowPrice" />}
+                    />
+                    <Field
+                      as={TextField}
+                      label="Крок ставки"
+                      name="bidIncrement"
+                      type="number"
+                      margin="normal"
+                      fullWidth
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">₴</InputAdornment>
+                        ),
+                      }}
+                      error={
+                        touched.bidIncrement && Boolean(errors.bidIncrement)
+                      }
+                      helperText={<ErrorMessage name="bidIncrement" />}
+                    />
+                    <Field
+                      as={TextField}
+                      label="Резервна ціна"
+                      name="reservePrice"
+                      type="number"
+                      margin="normal"
+                      fullWidth
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start">₴</InputAdornment>
+                        ),
+                      }}
+                      error={
+                        touched.reservePrice && Boolean(errors.reservePrice)
+                      }
+                      helperText={<ErrorMessage name="reservePrice" />}
+                    />
+                    <LoadingButton
+                      type="submit"
+                      variant="contained"
+                      loading={isLoading}
+                      disabled={isSubmitting}
+                      sx={{ mt: 3, mb: 2 }}
+                    >
+                      Оновити лот
+                    </LoadingButton>
+                  </Form>
+                )}
+              </Formik>
+            </Box>
+          </Grid>
+        </Box>
+      </Container>
+      <MessageSnackbar
+        open={openSuccessAlert}
+        onClose={() => setOpenSuccessAlert(false)}
+        severity="error"
+        message={successMessage}
+      />
+      <MessageSnackbar
+        open={openErrorAlert}
+        onClose={() => setOpenErrorAlert(false)}
+        severity="error"
+        message={
+          error?.data?.message ||
+          "Не вдалося оновити лот. Будь ласка, спробуйте ще раз."
+        }
+      />
+    </>
+  );
+};
+
+export default UpdateLot;
